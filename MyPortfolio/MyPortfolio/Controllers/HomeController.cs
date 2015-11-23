51a7f2f3edd378c0d4224b496332905824b70a13
pyxis828 <kirstinwalsh0805@gmail.com>
2015-11-23 01:51:21 -0500

    assignments

---
+++ MyPortfolio/MyPortfolio/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+             return View();
+        }
+
+        public ActionResult Contact()
+        {
+             return View();
+        }
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(EmailFormModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new MailMessage();
+                message.To.Add(new MailAddress("kirstinwalsh0805@gmail.com"));
+                message.From = new MailAddress(model.FromEmail);
+                message.Subject = model.Subject;
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+        public JsonResult GetData()
+        {
+            EmailFormModel efm = new EmailFormModel();
+            efm.FromName = "Kirstin";
+            efm.FromEmail = "kirstinwalsh0805@gmail.com";
+            efm.Subject = "From Server";
+
+            return Json(efm, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MyPortfolio/MyPortfolio/Controllers/PhotosController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MyPortfolio.Models;
+
+namespace MyPortfolio.Controllers
+{
+    public class PhotosController : Controller
+    {
+        private ProjectsEntities db = new ProjectsEntities();
+
+        // GET: Photos
+        public ActionResult Index()
+        {
+            var photos = db.Photos.Include(p => p.Project);
+            return View(photos.ToList());
+        }
+       
+
+        // GET: Photos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(photo);
+        }
+
+        // GET: Photos/Create
+        public ActionResult Create()
+        {
+            ViewBag.Project_ID = new SelectList(db.Projects, "ID", "Name");
+            return View();
+        }
+
+        // POST: Photos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Project_Name,Project_ID,Photo1,Photo2,Photo3,Photo4,Photo5,Photo6,Photo7,Photo8")] Photo photo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Photos.Add(photo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Project_ID = new SelectList(db.Projects, "ID", "Name", photo.Project_ID);
+            return View(photo);
+        }
+
+        // GET: Photos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Project_ID = new SelectList(db.Projects, "ID", "Name", photo.Project_ID);
+            return View(photo);
+        }
+
+        // POST: Photos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Project_Name,Project_ID,Photo1,Photo2,Photo3,Photo4,Photo5,Photo6,Photo7,Photo8")] Photo photo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(photo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Project_ID = new SelectList(db.Projects, "ID", "Name", photo.Project_ID);
+            return View(photo);
+        }
+
+        // GET: Photos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Photo photo = db.Photos.Find(id);
+            if (photo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(photo);
+        }
+
+        // POST: Photos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Photo photo = db.Photos.Find(id);
+            db.Photos.Remove(photo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
